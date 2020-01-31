3d2048fde1f01a0a657ce7be4f6c4628d76b1937
David Goosen <david@davidgoosen.kiwi>
2020-01-31 22:23:42 +1300

    Basic dialog clicking and text changing

---
+++ Assets/DialogSystem/DialogButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DialogButton : MonoBehaviour, IPointerClickHandler
+{
+    public DialogSystem m_System;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        Debug.Log("OnCLick!");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerClick");
+        m_System.TaskOnClick();
+        //m_Reply.text = "This is my reply";
+        //enemyHealth -= 1;
+    }
+
+    public void Register(DialogSystem dialogSystem)
+    {
+        m_System = dialogSystem;
+    }
+}
---
+++ Assets/DialogSystem/DialogSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DialogSystem : MonoBehaviour
+{
+    public int enemyHealth = 2;
+
+    public Button m_FirstButton;
+    public Text m_Reply;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //m_FirstButton.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void HandleAction(int selection)
+    {
+        if (selection == 1)
+        {
+        }
+    }
+
+    public void TaskOnClick()
+    {
+        Debug.Log("TaskOnClick");
+        m_Reply.text = "This is my reply";
+        enemyHealth -= 1;
+    }
+}
